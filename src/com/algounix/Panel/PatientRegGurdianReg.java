3b798ea44efc2b789448eb9d77aab559223d4c15
User <User@DESKTOP-LK7H3HF>
2025-05-23 21:03:46 +0530

    1. jButton11ActionPerformed :                reset() method call

--- src/com/algounix/Panel/PatientRegGurdianReg.java
+++ src/com/algounix/Panel/PatientRegGurdianReg.java
@@ -486,6 +486,10 @@ public class PatientRegGurdianReg extends javax.swing.JPanel {
                              + "VALUES ('"+pid+"' , '"+gid+"')");
                      
                      JOptionPane.showMessageDialog(this, "Guardian  Registered Successfully", "Warning" , JOptionPane.INFORMATION_MESSAGE);
+                     
+                     
+                      reset();
+                     
                  
                  }
                  
